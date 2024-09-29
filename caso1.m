930c9022cc5326fff9be46cef6f514f2c7418cb7
Luis Orihuela Orozco <103000057+LuisOrihuela08@users.noreply.github.com>
2024-09-29 12:27:31 -0500

    Create caso1.m

---
+++ caso1.m
@@ -0,0 +1,21 @@
+%DESARROLLO DEL CASO1
+v_deseada = 35;
+x=50:0.01:150;
+f=((9.8.*x./15) .* (1 - exp(-(15 *9) ./x)));
+plot(x,f,LineWidth=3,Color='b')
+hold on
+xlabel('EJE DE ABCISAS', FontSize=15,Color='m')
+ylabel('EJE DE ORDENADAS', FontSize=15, Color='m')
+yline(v_deseada, 'r--', 'LineWidth', 1.5)
+title('MÉTODO DE BISECCIÓN', FontSize=15, Color='m')
+grid on
+
+%CALCULANDO VALORES
+x=50;
+f=(9.8.*x./15) .* (1 - exp(-(15 *9) ./x))-35
+x=60;
+f=(9.8.*x./15) .* (1 - exp(-(15 *9) ./x))-35
+x=55;
+f=(9.8.*x./15) .* (1 - exp(-(15 *9) ./x))-35
+x=57.5;
+f=(9.8.*x./15) .* (1 - exp(-(15 *9) ./x))-35
